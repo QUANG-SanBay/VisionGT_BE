6b1bb7215a47d23309c39b8205742d50badfc095
QUANG-SanBay <quangsanbay@gmail.com>
2025-12-31 14:23:29 +0700

    feat: add models.py

--- traffic_signs/models.py
+++ traffic_signs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class TrafficSign(models.Model):
+    sign_Code = models.CharField(max_length=10, primary_key= True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    category = models.CharField(max_length=50)
+    image_url = models.URLField(max_length=200, null=True, blank=True)
+    penalty_details = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.name}"
+    
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser # Import AbstractUser to extend the default user model
+from django.db.models.signals import post_save # Import post_save signal để 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    # Add any additional fields you want for your custom user model here
+    ROLES_CHOICES = [
+        ('admin', 'Admin'),
+        ('customer', 'Customer'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLES_CHOICES, default='customer')
+    def __str__(self):
+        return str(self.username)
